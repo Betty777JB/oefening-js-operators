ddaf8f77b4f75189762c1bd7d000a149e26e2f8b
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-18 15:51:55 +0200

    einde oefening Comparison && logical Operators

---
+++ script.js
@@ -0,0 +1,45 @@
+const age = 25;
+
+if (age >= 18 && age <= 25) {
+
+    console.log("je krijg 50% korting");
+} else {
+
+    console.log("helaas ,geen korting");
+}
+
+ if (age >= 17 && age >= 26)  {
+
+ console.log("je krijg 50% korting");
+} else {
+
+console.log("helaas ,geen korting");
+}
+const name = ["Sarah" , "Bram" ];
+
+
+if (name === "Sarah"|| name ==="Bram") {
+    console.log("yeahh, gratis biertje");
+}
+
+else if  (name === "Betty" || name != "Anne" ){
+    console.log("helaas geen biertje voor jou");
+}
+
+const totalAmount = 100;
+
+if (totalAmount >= 100) {
+
+    console.log("champagne");
+}
+
+ else if (totalAmount > 50 ) {
+ console.log("portie nachos");
+}
+
+else if (totalAmount > 25 ) {
+console.log("portie bitterballen");
+}
+else {
+    console.log( "geen cadeau");
+}
